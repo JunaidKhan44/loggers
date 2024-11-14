ea5d1d078cf3f057f3e2a6a864f4e1788c6d0833
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-11-14 14:07:16 +0500

    Update HomeController.java

--- loggers/spotless-eg/src/main/java/com/jk/spotless/controller/HomeController.java
+++ loggers/spotless-eg/src/main/java/com/jk/spotless/controller/HomeController.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * @author Junaid.Khan
+ */
+ 
 @RestController
 public class HomeController {
 
